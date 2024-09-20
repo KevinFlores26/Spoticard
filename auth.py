8a7456b180713578fc5c739ed39e2845acc11ac1
kevinflores26 <kevinbrianflresh@gmail.com>
2024-09-20 10:49:28 -0600

    Reformat stable code in auth.py

--- auth.py
+++ auth.py
@@ -2,28 +2,33 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
 import webbrowser
 from spotipy.oauth2 import SpotifyOAuth
-from config import params as p
-from config import urls
 
-webbrowser.open(urls['AUTH_URL'])
+from config.config import params as p
+from config.config import urls
+
+webbrowser.open(urls["AUTH_URL"])
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        query_components = parse_qs(self.path.split('?', 1)[-1])
-        auth_code = query_components.get('code')
+        query_components = parse_qs(self.path.split("?", 1)[-1])
+        auth_code = query_components.get("code")
 
         if auth_code:
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(b"Authentication successful! You can close this window.")
             auth_code = auth_code[0]
 
-            # Intercambiar el código de autorización por un token de acceso
-            sp_oauth = SpotifyOAuth(client_id=p['CLIENT_ID'], client_secret=p['CLIENT_SECRET'], redirect_uri=urls['REDIRECT_URI'])
+            # Change the auth code into an access token
+            sp_oauth = SpotifyOAuth(
+                client_id=p["CLIENT_ID"],
+                client_secret=p["CLIENT_SECRET"],
+                redirect_uri=urls["REDIRECT_URI"],
+            )
             token_info = sp_oauth.get_access_token(auth_code)
-            print("Access Token: ", token_info['access_token'])
+            print("Access Token: ", token_info["access_token"])
 
         else:
             self.send_response(400)
@@ -31,7 +36,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Authentication failed.")
 
 
-server_address = ('', 8888)
+server_address = ("", 8888)
 httpd = HTTPServer(server_address, RequestHandler)
-print('Server running at http://localhost:8888/')
+print("Server running at http://localhost:8888/")
 httpd.serve_forever()
